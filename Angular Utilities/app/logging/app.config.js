64ad24da183f1ff60c13cd65e0961cb240131c60
Ahyari <ahmad.alhyari@gmail.com>
2015-12-25 15:39:31 +0400

    Add logging service

--- Angular Utilities/app/logging/app.config.js
+++ Angular Utilities/app/logging/app.config.js
@@ -1,14 +1,37 @@
 ﻿(function (module) {
     'use strict';
 
-    module.config(function ($provide) {
+    module.config(function ($provide, toastrConfig) {
 
         $provide.decorator("$exceptionHandler", function ($delegate, $injector) {
             return function (exception, cause) {
-                var $rootScope = $injector.get("$rootScope");
-                $rootScope.addError({ message: "Exception", reason: exception });
+                var loggingFactory = $injector.get("loggingFactory");
+                loggingFactory.addException(exception);
                 $delegate(exception, cause);
             };
+        });
+
+
+        angular.extend(toastrConfig, {
+            allowHtml: false,
+            closeButton: false,
+            closeHtml: '<button>&times;</button>',
+            extendedTimeOut: 1000,
+            iconClasses: {
+                error: 'toast-error',
+                info: 'toast-info',
+                success: 'toast-success',
+                warning: 'toast-warning'
+            },
+            messageClass: 'toast-message',
+            onHidden: null,
+            onShown: null,
+            onTap: null,
+            progressBar: false,
+            tapToDismiss: true,
+            timeOut: 5000,
+            titleClass: 'toast-title',
+            toastClass: 'toast'
         });
 
     });
--- Angular Utilities/app/logging/services/loggingFactory.js
+++ Angular Utilities/app/logging/services/loggingFactory.js
@@ -3,15 +3,63 @@
 
     module.factory('loggingFactory', loggingFactory);
 
-    loggingFactory.$inject = ['$http'];
+    loggingFactory.$inject = ['$http', 'toastr'];
 
-    function loggingFactory($http) {
+    function loggingFactory($http, toastr) {
+        var url = '/api/logging/addlog';
+        var logs = [];
+
         var service = {
-            getData: getData
+            addSuccess:addSuccess,
+            addInfo: addInfo,
+            addWarning: addWarning,
+            addError: addError,
+            addException: addException,
+            logs: logs
         };
 
         return service;
 
-        function addError() { };
+        //#region public functios
+        function addSuccess(message, pushToServer) {
+            addLog('success', message, pushToServer);
+            toastr.success(message);
+        };
+
+        function addInfo(message, pushToServer) {
+            addLog('info', message, pushToServer);
+            toastr.info(message);
+        };
+
+        function addWarning(message, pushToServer) {
+            addLog('warning', message, pushToServer);
+            toastr.warning(message);
+        };
+
+        function addError(message, pushToServer) {
+            addLog('error', message, pushToServer);
+            toastr.error(message);
+        };
+
+        function addException(message, pushToServer) {
+            addLog('exception', message, pushToServer);
+            toastr.error(message);
+        };
+        //#endregion
+
+        //#region private functios
+        function addLog(type, message, pushToServer) {
+            var data = { type: type, message: message };
+
+            logs.push(data);
+
+            if (pushToServer) {
+                $http.post(url, data)
+                .catch(function (error) {
+                    addException(error, false);
+                });
+            }
+        };
+        //#endregion
     }
 })(angular.module('utilities.logging'));
